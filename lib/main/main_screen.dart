0951b15b6ec325e469e54682eeed8216798ffa71
MuhammadRauf07 <raufdev202@gmail.com>
2024-01-30 23:58:35 +0500

    add bottom navbar

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -8,8 +8,55 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+
+
+  int _selectedIndex = 0;
+  final List<Widget> _tabs = [
+    const Text('Home'),
+    const Text('Search'),
+    const Text('Favorites'),
+    const Text('Settings'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _tabs.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.home),
+              onPressed: () => _onItemTapped(0),
+              color: _selectedIndex == 0 ? Colors.blue : Colors.grey,
+            ),
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () => _onItemTapped(1),
+              color: _selectedIndex == 1 ? Colors.blue : Colors.grey,
+            ),
+            IconButton(
+              icon: const Icon(Icons.favorite),
+              onPressed: () => _onItemTapped(2),
+              color: _selectedIndex == 2 ? Colors.blue : Colors.grey,
+            ),
+            IconButton(
+              icon: const Icon(Icons.settings),
+              onPressed: () => _onItemTapped(3),
+              color: _selectedIndex == 2 ? Colors.blue : Colors.grey,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
